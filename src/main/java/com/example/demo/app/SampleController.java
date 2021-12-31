d42163ae2c404199e40b34e1a76ec852abc84b05
akrmyuc <87920616+akrmyuc@users.noreply.github.com>
2021-12-31 23:29:25 +0900

    update: DB操作のコードを追加

--- src/main/java/com/example/demo/app/SampleController.java
+++ src/main/java/com/example/demo/app/SampleController.java
@@ -1,5 +1,9 @@
 package com.example.demo.app;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,17 +15,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/sample")
 public class SampleController {
+	// jdbcでDBを操作するためのクラスを初期化
+	private final JdbcTemplate jdbcTemplate;
 
-// 	private final JdbcTemplate jdbcTemplate;
-
-// 	//Add an annotation here
-// 	public SampleController(JdbcTemplate jdbcTemplate) {
-// 		this.jdbcTemplate = jdbcTemplate;
-// 	}
+//	Add an annotation here
+	@Autowired
+ 	public SampleController(JdbcTemplate jdbcTemplate) {
+ 		this.jdbcTemplate = jdbcTemplate;
+ 	}
 
 	// GET:データをURLに含める
 	@GetMapping("/test")
 	public String test(Model model) {
+		String sql = "select id, name, email "
+				+ "from inquiry where id = 1";
+
+		Map<String, Object> map = jdbcTemplate.queryForMap(sql);
+
+		// Mapの値をhtmlに渡す
+		model.addAttribute("name", map.get("name"));
+		model.addAttribute("email", map.get("email"));
 
 		model.addAttribute("title", "Inquiry Form");
 
