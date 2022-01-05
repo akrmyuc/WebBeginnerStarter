a7e2d471e47b2778e6d28528a4c941c4f49b3ee3
akrmyuc <87920616+akrmyuc@users.noreply.github.com>
2022-01-05 20:31:02 +0900

    fix: エラー処理をコメントアウト

--- src/main/java/com/example/demo/app/inquiry/InquiryController.java
+++ src/main/java/com/example/demo/app/inquiry/InquiryController.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.demo.entity.Inquiry;
-import com.example.demo.service.InquiryNotFoundException;
 import com.example.demo.service.InquiryService;
 
 @Controller
@@ -109,10 +107,10 @@ public class InquiryController {
 		return "redirect:/inquiry/form"; // URL
 	}
 
-//	例外を補足するメソッド
-	@ExceptionHandler(InquiryNotFoundException.class)
-	public String handleException(InquiryNotFoundException e, Model model) {
-		model.addAttribute("message", e);
-		return "error/CustomPage";
-	}
+////	例外を補足するメソッド
+//	@ExceptionHandler(InquiryNotFoundException.class)
+//	public String handleException(InquiryNotFoundException e, Model model) {
+//		model.addAttribute("message", e);
+//		return "error/CustomPage";
+//	}
 }
